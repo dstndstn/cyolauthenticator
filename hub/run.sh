5e3c26935ac045075d56a13ea561ae2470a60f2b
Dustin Lang <dstndstn@gmail.com>
2020-10-15 14:45:23 -0400

    move adding NFS on kubes until after NFS mount point is exported; add rsyslog

--- hub/run.sh
+++ hub/run.sh
@@ -3,6 +3,7 @@
 cd /
 
 service ssh start
+service rsyslog start
 
 # Set this to the Google Cloud Platform project name
 
@@ -30,10 +31,6 @@ gcloud config set compute/zone $GCP_ZONE
 
 gcloud container clusters get-credentials $CLUSTER_NAME
 
-## If you create a new Kubernetes cluster, you must do:
-kubectl apply -f nfs-pv.yaml
-kubectl apply -f nfs-pvc.yaml
-
 #gcloud config set container/use_client_certificate True
 
 # (Re-)add firewall entries.  This complains if they already exist.
@@ -44,6 +41,21 @@ gcloud compute firewall-rules create allow-2222 --direction=INGRESS --priority=1
 gcloud compute firewall-rules create allow-80   --direction=INGRESS --priority=1000 --network="$NET" --action=ALLOW --rules=tcp:80   --source-ranges=0.0.0.0/0
 gcloud compute firewall-rules create allow-443  --direction=INGRESS --priority=1000 --network="$NET" --action=ALLOW --rules=tcp:443  --source-ranges=0.0.0.0/0
 gcloud compute firewall-rules create kube-to-all-vms-on-network --network="$NET" --source-ranges="$IP" --allow=tcp,udp,icmp,esp,ah,sctp
+
+# Need to run in a "privileged" container for this!!
+mount -t nfsd nfsd /proc/fs/nfsd
+
+mkdir -p /run/sendsigs.omit.d/
+service rpcbind start
+service nfs-common start
+service nfs-kernel-server start
+
+exportfs -a
+
+## If you create a new Kubernetes cluster, you must do:
+kubectl apply -f nfs-pv.yaml
+kubectl apply -f nfs-pvc.yaml
+
 
 # Reset the LDAP passwords
 slappasswd -g > /tmp/slappasswd
@@ -72,14 +84,6 @@ else
     ldapaddgroup users
 fi
 rm /tmp/slappasswd
-
-# Need to run in a "privileged" container for this!!
-mount -t nfsd nfds /proc/fs/nfsd
-
-mkdir -p /run/sendsigs.omit.d/
-service rpcbind start
-service nfs-common start
-service nfs-kernel-server start
 
 for ((;;)); do
   # If we have a certificate directory...
