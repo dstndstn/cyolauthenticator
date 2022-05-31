0655c05bffa69e94b4f817362f074c78f6a07364
Dustin Lang <dstndstn@gmail.com>
2022-05-31 12:51:31 -0400

    Update inituser-root.sh

--- hub/inituser-root.sh
+++ hub/inituser-root.sh
@@ -6,3 +6,6 @@ echo User $user
 
 setquota -u $user 5G 5G 500k 500k /nfs
 
+# In case the username previously existed and was deleted, make sure the home directory is chowned.
+# (could also rm -R the previous contents...)
+chown -R $user /nfs/home/$user
